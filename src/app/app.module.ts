033833a36aad04d73350803cd9890b4d231a86f5
Denis Hasanenko <denis.hasanenko@gmail.com>
2020-05-04 19:44:44 +0300

    angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatSliderModule} from '@angular/material';
+import { MatSliderModule } from '@angular/material/slider';
 import {MainComponent} from './main/main.component';
 import {PetsModule} from './pets/pets.module';
 import {GraphqlModule} from './graphql/graphql.module';
